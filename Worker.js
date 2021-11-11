548a35e34b4909331b6c0e0743f95eb668ce6c54
Valentin <44469298+Infinity-Kim@users.noreply.github.com>
2021-11-11 19:29:31 +0300

    end task#3

--- Worker.js
+++ Worker.js
@@ -1,5 +1,33 @@
 class Worker {
-    constructor() {
+    #name;
+    #surname;
+    #rate;
+    #days;
 
+    constructor(name, surname, rate, days) {
+        this.#name = name;
+        this.#surname = surname;
+        this.#rate = +rate;
+        this.#days = +days;
+    }
+
+    get getName() {
+        return this.#name;
+    }
+
+    get getSurname() {
+        return this.#surname;
+    }
+
+    get getRate() {
+        return this.#rate;
+    }
+
+    get getDays() {
+        return this.#days;
+    }
+
+    get getSalary() {
+        return this.#rate * this.#days;
     }
 }
