764e1710fcdf554482881fe77ecdfff2c289cf1e
Valentin <44469298+Infinity-Kim@users.noreply.github.com>
2021-11-11 17:48:52 +0300

    add solve task#1

--- Elem.js
+++ Elem.js
@@ -1,7 +1,27 @@
 class Elem {
+
     constructor(selector) {
-        this.selector = selector;
+        this.element = document.querySelector(selector);
     }
 
+    html(text) {
+        this.element.innerHTML = text;
+        return this;
+    }
+
+    attr(name, value) {
+        this.element.setAttribute(name, value);
+        return this;
+    }
+
+    append(text) {
+        this.element.append(text);
+        return this;
+    }
+
+    prepend(text) {
+        this.element.prepend(text);
+        return this;
+    }
 
 }
