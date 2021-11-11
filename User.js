18b8afcc62b24d84932eeb80e45b44271e5c46c0
Valentin <44469298+Infinity-Kim@users.noreply.github.com>
2021-11-11 18:44:40 +0300

    end task#2

--- User.js
+++ User.js
@@ -1,15 +1,23 @@
+// Сделал геттеры, так как, в примере вызова был вызов без скобок
+
 class User {
     constructor(name, surname) {
         this.name = name;
         this.surname = surname;
     }
 
-    // getFullName напишите сами
+    get getFullName() {
+        return `${this.name} ${this.surname}`;
+    }
 }
 
 class Student extends User {
-    constructor(year) {
-        super();
-        this.year = year;
+    constructor(name, surname, year) {
+        super(name, surname);
+        this.year = +year;
+    }
+
+    get getCourse () {
+        return new Date().getFullYear() - this.year;
     }
 }
